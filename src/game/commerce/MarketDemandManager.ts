6226f56f78493eff97b8c94ea6686aff206c9850
David <david.annebicque@gmail.com>
2026-07-21 20:44:02 +0200

    refactor: remove obsolete demand facade

--- src/game/commerce/MarketDemandManager.ts
+++
@@ -1,7 +0,0 @@
-export { MarketDemandManager } from '@market-tycoon/economy'
-export type {
-  CustomerPriceContext,
-  MarketDemandOptions,
-  PurchaseDecision,
-  PurchaseDecisionResult,
-} from '@market-tycoon/economy'
