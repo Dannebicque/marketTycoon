77a9a7f46ba37667e0f1f77b7223ad353fdb41e4
David <david.annebicque@gmail.com>
2026-07-21 10:05:26 +0200

    Ajouter la pizza surgelée autonome

---
+++ src/game/catalog/products/frozen/frozenPizza.product.ts
@@ -0,0 +1,2 @@
+import { defineProduct } from '../../../definitions'
+export default defineProduct({ key: 'frozen-pizza', category: 'frozen', name: 'Pizza surgelée', shortName: 'PIZZ', salePrice: 7, purchasePrice: 3.8, color: 0x60a5fa, requiresFreezing: true })
