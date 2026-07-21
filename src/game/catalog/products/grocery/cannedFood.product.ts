f5b7b852df79620eb8f7f1f193f9c337e366bddb
David <david.annebicque@gmail.com>
2026-07-21 10:03:54 +0200

    Ajouter les conserves autonomes

---
+++ src/game/catalog/products/grocery/cannedFood.product.ts
@@ -0,0 +1,3 @@
+import { defineProduct } from '../../../definitions'
+
+export default defineProduct({ key: 'canned-food', category: 'grocery', name: 'Conserves', shortName: 'CONS', salePrice: 4.5, purchasePrice: 2.1, color: 0xb07a4f })
