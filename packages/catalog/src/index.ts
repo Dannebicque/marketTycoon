a06db640674e05557f0f175c4cc2ca59889109ff
David <david.annebicque@gmail.com>
2026-07-21 20:30:03 +0200

    feat(catalog): expose catalog API

---
+++ packages/catalog/src/index.ts
@@ -0,0 +1,12 @@
+export { CatalogRegistry } from './CatalogRegistry'
+export type {
+  CompartmentType,
+  PaymentMethod,
+  ProductCatalogReader,
+  ProductCategory,
+  ProductDefinition,
+  StorageDefinition,
+  StorageType,
+  SupplierCatalogReader,
+  SupplierDefinition,
+} from './contracts'
