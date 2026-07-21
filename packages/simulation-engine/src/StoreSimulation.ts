8a8580f68b7d66c3246306ac2e5380cc2b952e7d
David <david.annebicque@gmail.com>
2026-07-21 22:06:58 +0200

    Préparer l’extraction de StoreSimulation

---
+++ packages/simulation-engine/src/StoreSimulation.ts
@@ -0,0 +1,2 @@
+export { StoreSimulation } from '../../../src/game/StoreSimulation'
+export type { BasketLine, BasketSummary, DaySnapshot, ShoppingPlanItem, StoreMetrics } from '../../../src/game/StoreSimulation'
