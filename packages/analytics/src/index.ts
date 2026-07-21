1cc00bf9f60e638152207bb4349f47c0df122ecf
David <david.annebicque@gmail.com>
2026-07-21 23:58:48 +0200

    Exporter les statistiques de visites clients

--- packages/analytics/src/index.ts
+++ packages/analytics/src/index.ts
@@ -8,3 +8,15 @@ export type {
   PurchaseDecision,
   PurchaseDecisionResult,
 } from './CustomerAnalyticsManager'
+export { CustomerVisitAnalyticsManager } from './CustomerVisitAnalyticsManager'
+export type {
+  CustomerAbandonReason,
+  CustomerProfileVisitAnalytics,
+  CustomerVisitAbandoned,
+  CustomerVisitAnalyticsState,
+  CustomerVisitCompleted,
+  CustomerVisitOutcome,
+  CustomerVisitRecord,
+  CustomerVisitStarted,
+  CustomerVisitSummary,
+} from './CustomerVisitAnalyticsManager'
