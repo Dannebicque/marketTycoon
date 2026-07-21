4127504ca0086b9456fa35895ce36100aa22e1f7
David <david.annebicque@gmail.com>
2026-07-21 23:41:48 +0200

    Publier les API de profils clients

--- packages/customers/src/index.ts
+++ packages/customers/src/index.ts
@@ -1,6 +1,9 @@
 export { Customer } from './Customer'
 export { CustomerBasket, summarizeCustomerBasket } from './CustomerBasket'
 export { CustomerJourney } from './CustomerJourney'
+export { CustomerSatisfaction } from './CustomerSatisfaction'
+export { createCustomerProfile } from './CustomerProfileFactory'
+export { CUSTOMER_PROFILE_CATALOG } from './CustomerProfileCatalog'
 export {
   calculateCheckoutSatisfaction,
   calculateQueueSatisfaction,
@@ -8,9 +11,18 @@ export {
   isCustomerImpatient,
 } from './CustomerPolicies'
 export type {
+  CustomerProfileDefinition,
+  CustomerProfileKey,
+} from './CustomerProfileCatalog'
+export type {
+  CustomerProfileFactoryOptions,
+} from './CustomerProfileFactory'
+export type {
   CustomerBasketLine,
   CustomerBasketSummary,
   CustomerJourneyState,
   CustomerProfile,
+  CustomerSatisfactionBreakdown,
+  CustomerSatisfactionFactor,
   CustomerSnapshot,
 } from './contracts'
