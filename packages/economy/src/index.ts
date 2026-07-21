643a834834d2f2a8baaf13e3516709ec2ecc5551
David <david.annebicque@gmail.com>
2026-07-21 19:10:09 +0200

    Exposer l’API publique économie

---
+++ packages/economy/src/index.ts
@@ -0,0 +1,9 @@
+export { StorePricingManager } from '../../../src/game/pricing/StorePricingManager'
+export { MarketDemandManager } from '../../../src/game/commerce/MarketDemandManager'
+export type {
+  CustomerPriceContext,
+  PurchaseDecision,
+  PurchaseDecisionResult,
+} from '../../../src/game/commerce/MarketDemandManager'
+export { PurchaseOrderManager } from '../../../src/game/logistics/PurchaseOrderManager'
+export { ReserveManager } from '../../../src/game/logistics/ReserveManager'
