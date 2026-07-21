c299ae941338e2e77be4c108a654a8039f896185
David <david.annebicque@gmail.com>
2026-07-21 19:09:46 +0200

    Exposer l’API publique du moteur de simulation

---
+++ packages/simulation-engine/src/index.ts
@@ -0,0 +1,16 @@
+export { StoreSimulation } from '../../../src/game/StoreSimulation'
+export type {
+  BasketLine,
+  BasketSummary,
+  DaySnapshot,
+  ShoppingPlanItem,
+  StoreMetrics,
+} from '../../../src/game/StoreSimulation'
+export { GridManager } from '../../../src/game/GridManager'
+export type {
+  Direction,
+  GridCell,
+  PlacedBuilding,
+  PlacedEdge,
+} from '../../../src/game/GridManager'
+export { NavigationGrid } from '../../../src/game/NavigationGrid'
