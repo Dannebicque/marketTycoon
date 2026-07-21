0fa2f3dff60207f30de560dc0029184232def9f6
David <david.annebicque@gmail.com>
2026-07-21 10:05:35 +0200

    Ajouter les glaces autonomes

---
+++ src/game/catalog/products/frozen/iceCream.product.ts
@@ -0,0 +1,2 @@
+import { defineProduct } from '../../../definitions'
+export default defineProduct({ key: 'ice-cream', category: 'frozen', name: 'Glaces', shortName: 'GLACE', salePrice: 6, purchasePrice: 3, color: 0xf9a8d4, requiresFreezing: true })
