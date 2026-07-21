ab0464ce7e374b4edb8eeff7e9906015854ab890
David <david.annebicque@gmail.com>
2026-07-21 19:11:13 +0200

    Exposer l’API publique événements

---
+++ packages/events/src/index.ts
@@ -0,0 +1,3 @@
+export { GameEventBus } from '../../../src/game/events/GameEventBus'
+export { gameEvents } from '../../../src/game/events/gameEvents'
+export type { MarketTycoonEvents } from '../../../src/game/events/gameEvents'
