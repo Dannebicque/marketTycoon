85567c2cebcdf1f74de1f910385589345f5727d3
David <david.annebicque@gmail.com>
2026-07-21 10:04:05 +0200

    Ajouter les pommes autonomes

---
+++ src/game/catalog/products/fruits/apple.product.ts
@@ -0,0 +1,2 @@
+import { defineProduct } from '../../../definitions'
+export default defineProduct({ key: 'apple', category: 'fruit', name: 'Pommes', shortName: 'POM', salePrice: 3, purchasePrice: 1.3, color: 0xef4444, shelfLifeDays: 5 })
