79f38d5458885df02efa14acc6fa5283fcad10b8
David <david.annebicque@gmail.com>
2026-07-21 10:04:56 +0200

    Ajouter l’eau autonome

---
+++ src/game/catalog/products/drinks/water.product.ts
@@ -0,0 +1,2 @@
+import { defineProduct } from '../../../definitions'
+export default defineProduct({ key: 'water', category: 'drink', name: 'Eau', shortName: 'EAU', salePrice: 2, purchasePrice: .7, color: 0x38bdf8 })
