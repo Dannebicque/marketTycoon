931a18c862d05a13db91f476b15a54901eccceed
David <david.annebicque@gmail.com>
2026-07-21 10:33:30 +0200

    Définir les capacités des sodas

--- src/game/catalog/products/drinks/soda.product.ts
+++ src/game/catalog/products/drinks/soda.product.ts
@@ -1,2 +1,6 @@
 import { defineProduct } from '../../../definitions'
-export default defineProduct({ key: 'soda', category: 'drink', name: 'Sodas', shortName: 'SODA', salePrice: 4, purchasePrice: 1.8, color: 0x0ea5e9 })
+export default defineProduct({
+  key: 'soda', category: 'drink', name: 'Sodas', shortName: 'SODA',
+  salePrice: 4, purchasePrice: 1.8, color: 0x0ea5e9,
+  capacities: { 'standard-shelf': 18 },
+})
