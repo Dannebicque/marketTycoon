38fd1b73469a539fc3d8f395c8d77cffbf15c2ae
David <david.annebicque@gmail.com>
2026-07-21 10:03:45 +0200

    Découper les produits en définitions autonomes

---
+++ src/game/catalog/products/grocery/pasta.product.ts
@@ -0,0 +1,3 @@
+import { defineProduct } from '../../../definitions'
+
+export default defineProduct({ key: 'pasta', category: 'grocery', name: 'Pâtes', shortName: 'PÂTES', salePrice: 3.5, purchasePrice: 1.4, color: 0xd6a75f })
