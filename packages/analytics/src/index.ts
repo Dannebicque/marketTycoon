f99244f952dfad9e2e179a58b1e8a227a8876a28
David <david.annebicque@gmail.com>
2026-07-21 19:10:47 +0200

    Exposer l’API publique analytics

---
+++ packages/analytics/src/index.ts
@@ -0,0 +1,6 @@
+export { CustomerAnalyticsManager } from '../../../src/game/analytics/CustomerAnalyticsManager'
+export type {
+  CustomerAnalyticsSummary,
+  CustomerPurchaseObservation,
+  ProductCustomerAnalytics,
+} from '../../../src/game/analytics/CustomerAnalyticsManager'
