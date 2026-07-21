cf683b7dbd2c23bcacea3a9333cd42481d69dfd9
David <david.annebicque@gmail.com>
2026-07-21 10:05:46 +0200

    Ajouter le pain autonome

---
+++ src/game/catalog/products/bakery/bread.product.ts
@@ -0,0 +1,2 @@
+import { defineProduct } from '../../../definitions'
+export default defineProduct({ key: 'bread', category: 'bakery', name: 'Pain', shortName: 'PAIN', salePrice: 2.5, purchasePrice: .8, color: 0xc08457, shelfLifeDays: 2 })
