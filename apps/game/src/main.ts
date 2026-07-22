f3ed4e915d09622b4b140a46d8f1ea2a2dc926c4
David <david.annebicque@gmail.com>
2026-07-22 13:35:33 +0200

    Démarrer la projection de progression

--- apps/game/src/main.ts
+++ apps/game/src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { i18n } from './i18n'
+import { progression } from './progression'
 import './style.css'
 
 createApp(App)
+  .provide('progression', progression)
   .use(i18n)
   .mount('#app')
