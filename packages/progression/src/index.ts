04f92b575aa498555a36085108dd77d030c08b9f
David <david.annebicque@gmail.com>
2026-07-22 18:31:28 +0200

    feat(progression): export unlock access policy

--- packages/progression/src/index.ts
+++ packages/progression/src/index.ts
@@ -1,4 +1,6 @@
 export { ProgressionManager } from './ProgressionManager'
+export { ProgressionAccessPolicy } from './ProgressionAccessPolicy'
+export type { ProgressionAvailability, ProgressionUnlockable } from './ProgressionAccessPolicy'
 export { DEFAULT_PROGRESSION_CONFIGURATION, DEFAULT_PROGRESSION_LEVELS, DEFAULT_PROGRESSION_OBJECTIVES } from './defaultProgression'
 export { formatProgressionConfigurationIssues, parseProgressionConfiguration } from './configuration'
 export type * from './contracts'
