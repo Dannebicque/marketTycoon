4a19f2e2fe8228728ae1efa01414b2840a1816d5
David <david.annebicque@gmail.com>
2026-07-22 22:50:42 +0200

    Exporter le domaine des zones

---
+++ packages/store-zones/src/index.ts
@@ -0,0 +1,4 @@
+export { StoreZoneManager } from './StoreZoneManager'
+export { ZONES, getZoneDefinition } from './catalog'
+export { defineZone } from './contracts'
+export type { ZoneCell, ZoneConstraints, ZoneCostDefinition, ZoneDefinition, ZoneSummary } from './contracts'
