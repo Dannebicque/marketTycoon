031466daa3326075a0641383d0ccdd2d42f5584e
David <david.annebicque@gmail.com>
2026-07-22 17:55:13 +0200

    Exporter la configuration de progression

--- packages/progression/src/index.ts
+++ packages/progression/src/index.ts
@@ -1,3 +1,4 @@
 export { ProgressionManager } from './ProgressionManager'
-export { DEFAULT_PROGRESSION_LEVELS, DEFAULT_PROGRESSION_OBJECTIVES } from './defaultProgression'
+export { DEFAULT_PROGRESSION_CONFIGURATION, DEFAULT_PROGRESSION_LEVELS, DEFAULT_PROGRESSION_OBJECTIVES } from './defaultProgression'
+export { formatProgressionConfigurationIssues, parseProgressionConfiguration } from './configuration'
 export type * from './contracts'
