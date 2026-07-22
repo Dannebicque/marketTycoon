0a8a2d50582e6c1cc6c696f9192ae9ab23070958
davidannebicque <david.annebicque@gmail.com>
2026-07-22 17:50:47 +0200

    fix: import vite

--- apps/game/vite.config.ts
+++ apps/game/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
       '@market-tycoon/save': packageSource('save'),
       '@market-tycoon/catalog': packageSource('catalog'),
       '@market-tycoon/customers': packageSource('customers'),
+      '@market-tycoon/progression': packageSource('progression'),
     },
   },
 })
