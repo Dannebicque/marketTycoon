eb7c11caf63e4a825943749d0058450e16ecb058
David <david.annebicque@gmail.com>
2026-07-23 16:39:37 +0200

    Exporter le moteur de tâches employés

--- packages/employees/src/index.ts
+++ packages/employees/src/index.ts
@@ -1,8 +1,12 @@
 export { EmployeeManager } from './EmployeeManager'
+export { EmployeeTaskQueue } from './EmployeeTaskQueue'
 export type {
   EmployeeState,
   EmployeeStatus,
   EmployeeTaskState,
   EmployeeTaskType,
+  EmployeeWorkTask,
+  EmployeeWorkTaskInput,
+  EmployeeWorkTaskStatus,
 } from './employeeTypes'
 export type { EmployeeRoleDefinition, EmployeeRoleKey } from '@market-tycoon/catalog'
