9d3641907fda686f3c0035d893913e90ab2d9411
David <david.annebicque@gmail.com>
2026-07-24 15:33:09 +0200

    feat(world-map): expose public API

---
+++ packages/world-map/src/index.ts
@@ -0,0 +1,20 @@
+export { JsonWorldMapSource, WorldMapLoader } from './WorldMapLoader'
+export { requireValidWorldMap, validateWorldMap } from './WorldMapValidator'
+export type { WorldMapSource } from './WorldMapLoader'
+export type { WorldMapValidationResult } from './WorldMapValidator'
+export type {
+  InitialEdgeDefinition,
+  InitialEquipmentDefinition,
+  MapPoint,
+  MapRect,
+  MapSize,
+  ParcelAccess,
+  ParcelDefinition,
+  ParcelUsage,
+  RoadDefinition,
+  SpawnPointDefinition,
+  StoreSeedDefinition,
+  WorldBuildingDefinition,
+  WorldBuildingOwner,
+  WorldMapDefinition,
+} from './contracts'
