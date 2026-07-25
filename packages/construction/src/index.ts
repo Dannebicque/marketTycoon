709823493dd8784d8c1679d67e9d743c8a9a0025
David <david.annebicque@gmail.com>
2026-07-25 07:13:44 +0200

    refactor(construction): export surface domain

--- packages/construction/src/index.ts
+++ packages/construction/src/index.ts
@@ -1,6 +1,8 @@
+export { BuildSurfaceMap, floodFillCells, rectangleCells } from './BuildSurfaceMap'
 export { ConstructionOrderQueue } from './ConstructionOrder'
 export { MaterialCatalog, constructionMaterialCatalog, DEFAULT_CONSTRUCTION_MATERIALS } from './MaterialCatalog'
 export { createRectangleBounds, getRectangleCells, getRectanglePerimeterUnits } from './ConstructionPreview'
+export type { BuildSurfaceCell, BuildSurfaceSnapshot } from './BuildSurfaceMap'
 export type { ConstructionOrder, ConstructionOrderKind, ConstructionOrderStatus } from './ConstructionOrder'
 export type { ConstructionMaterialDefinition, ConstructionMaterialKind } from './MaterialCatalog'
 export type { ConstructionPoint, ConstructionPreview, ConstructionRect } from './ConstructionPreview'
