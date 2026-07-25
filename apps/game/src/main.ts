bf02205d4af34d6ebb75a653b313edca06e3e3ba
David <david.annebicque@gmail.com>
2026-07-25 07:32:27 +0200

    feat(construction): install structural build tools

--- apps/game/src/main.ts
+++ apps/game/src/main.ts
@@ -19,6 +19,7 @@ import { installBuildModeHistory } from './phaser/installBuildModeHistory'
 import { installBuildToolController } from './phaser/installBuildToolController'
 import { installConstructionPreview } from './phaser/installConstructionPreview'
 import { installOwnedLandOverlay } from './phaser/installOwnedLandOverlay'
+import { installStructuralBuildTools } from './phaser/installStructuralBuildTools'
 import { installSurfaceBuildTools } from './phaser/installSurfaceBuildTools'
 import { installViewDisplay } from './phaser/installViewDisplay'
 import { installWorldMapPhaserAdapter } from './phaser/installWorldMapAdapter'
@@ -42,7 +43,7 @@ import './zones.css'
 import './help.css'
 import './simulation-context.css'
 
-const DEV_SCENARIO_VERSION = '19'
+const DEV_SCENARIO_VERSION = '20'
 const DEV_SCENARIO_VERSION_KEY = 'market-tycoon.dev-scenario-version'
 
 async function bootstrap() {
@@ -53,6 +54,7 @@ async function bootstrap() {
   installBuildToolController()
   installBuildInteractionTools()
   installSurfaceBuildTools()
+  installStructuralBuildTools()
   installConstructionPreview()
   installOwnedLandOverlay()
   installBuildingIdentityOverlay()
